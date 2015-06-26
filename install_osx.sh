94009a781bc640fdbd54b96c2c806f994ea85d1d
smithsf0x <f0x@MacBook.fritz.box>
2015-06-26 11:13:54 +0200

    Fixed

--- install_osx.sh
+++ install_osx.sh
@@ -2,21 +2,25 @@
 
 #// smithsf0x
 
+
 err_ident=null
+
+argument=$1
 
 install_psp2sdk(){
     export PATH=$DEVKITARM/bin:$PATH
-    if [ "$" == "--show" ] 
+    if [ "$argument" == "--show" ] 
         then
             ./autogen.sh --prefix=$PSP2SDK
             cd src && make install
             cd ..
             cd tools && make install
+            clear
         else
-            
             echo -ne "|Running autogen.sh with Prefix:   "
-            ./autogen.sh --prefix=$PSP2SDK >& /dev/null && err_ident=$?
-            if [ err_ident ]
+            ./autogen.sh --prefix=$PSP2SDK >& /dev/null
+            err_ident=$?
+            if [ $err_ident == 0 ]
                 then
                     echo "DONE"
                 else
@@ -24,18 +28,19 @@ install_psp2sdk(){
             fi
 
             echo -ne "|Making and Installing PSP2SDK:    "
-            cd src && make install >& /dev/null && err_ident=$?
-            if [ err_ident ]
+            cd src && make install >& /dev/null
+            err_ident=$?
+            if [ $err_ident == 0 ]
                 then
                     echo "DONE"
                 else
                     echo "ERROR"
             fi
 
-            cd ..
             echo -ne "|Making and Installing psp2-fixup: "
-            cd tools && make install >& /dev/null && err_ident=$?
-            if [ err_ident ]
+            cd ../tools && make install >& /dev/null
+            err_ident=$?
+            if [ $err_ident == 0 ]
                 then
                     echo "DONE"
                 else
@@ -48,3 +53,4 @@ install_psp2sdk
 echo "|------------------------------|"
 echo "| Thanks to doobz for his help |"
 echo "|----------~FINISHED~----------|"
+echo "|______________________________|"
