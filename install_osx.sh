b8ffa450035cf188677d0498b71a8fd1dfbf818e
smithsf0x <f0x@MacBook.fritz.box>
2015-06-26 03:45:15 +0200

    Fixed

--- install_osx.sh
+++ install_osx.sh
@@ -5,39 +5,42 @@
 err_ident=null
 
 install_psp2sdk(){
-	export PATH=$DEVKITARM/bin:$PATH
-	if [ "$1" == "--show" ] 
-		then
-			./autogen.sh --prefix=$PSP2SDK
-   			cd src && make install
-    		cd ..
-    		cd tools && make install
-    	else
+    export PATH=$DEVKITARM/bin:$PATH
+    if [ "$" == "--show" ] 
+        then
+            ./autogen.sh --prefix=$PSP2SDK
+            cd src && make install
+            cd ..
+            cd tools && make install
+        else
             
-    		echo -ne "|Running autogen.sh with Prefix:   "
-    		./autogen.sh --prefix=$PSP2SDK >& /dev/null
-    		echo "DONE"
-    		echo -ne "|Making and Installing PSP2SDK:    "
-   			cd src && make install >& /dev/null && err_ident=$?
-
-            if [ ! err_ident ]
+            echo -ne "|Running autogen.sh with Prefix:   "
+            ./autogen.sh --prefix=$PSP2SDK >& /dev/null && err_ident=$?
+            if [ err_ident ]
                 then
                     echo "DONE"
                 else
                     echo "ERROR"
             fi
-            err_ident=null
-    		cd ..
-    		echo -ne "|Making and Installing psp2-fixup: "
-    		cd tools && make install >& /dev/null && err_ident=$?
 
-            if [ ! err_ident ]
+            echo -ne "|Making and Installing PSP2SDK:    "
+            cd src && make install >& /dev/null && err_ident=$?
+            if [ err_ident ]
                 then
                     echo "DONE"
                 else
                     echo "ERROR"
             fi
-            err_ident=null
+
+            cd ..
+            echo -ne "|Making and Installing psp2-fixup: "
+            cd tools && make install >& /dev/null && err_ident=$?
+            if [ err_ident ]
+                then
+                    echo "DONE"
+                else
+                    echo "ERROR"
+            fi
     fi
 }
 
