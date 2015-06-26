d48548037f5056902a55fd5c89ca847d3bffff9d
smithsf0x <f0x@MacBook.fritz.box>
2015-06-26 03:30:13 +0200

    error handler [NOT TESTED]

--- install_osx.sh
+++ install_osx.sh
@@ -2,6 +2,8 @@
 
 #// smithsf0x
 
+err_ident=null
+
 install_psp2sdk(){
 	export PATH=$DEVKITARM/bin:$PATH
 	if [ "$1" == "--show" ] 
@@ -16,13 +18,26 @@ install_psp2sdk(){
     		./autogen.sh --prefix=$PSP2SDK >& /dev/null
     		echo "DONE"
     		echo -ne "|Making and Installing PSP2SDK:    "
-   			cd src && make install >& /dev/null
-   			echo "DONE"
-    		cd ..
+   			cd src && make install >& /dev/null && err_ident=$?
 
+            if [ ! err_ident ]
+                then
+                    echo "DONE"
+                else
+                    echo "ERROR"
+            fi
+            err_ident=null
+    		cd ..
     		echo -ne "|Making and Installing psp2-fixup: "
-    		cd tools && make install >& /dev/null
-    		echo "DONE"
+    		cd tools && make install >& /dev/null && err_ident=$?
+
+            if [ ! err_ident ]
+                then
+                    echo "DONE"
+                else
+                    echo "ERROR"
+            fi
+            err_ident=null
     fi
 }
 
